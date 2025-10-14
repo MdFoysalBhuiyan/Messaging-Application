2fe0e951836d5ec539271cd39be2364963911df4
Saishab Sarkar <soishobsarkar@gmail.com>
2025-10-14 10:59:55 +0600

    Add files via upload
    Added new features:
    1. User can select item from combo box 
    2. Can see the bill via printing

---
+++ Print.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace message // Ensure this namespace matches your project
+{
+    public partial class Print : Form
+    {
+        // Declare controls at the class level to access them in multiple methods
+        private ComboBox partsComboBox;
+        private RichTextBox receiptBox;
+        private Dictionary<string, int> partsData;
+
+        public Print()
+        {
+            InitializeComponent();
+            this.Text = "Receipt Printer";
+            this.Size = new Size(400, 600);
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.BackColor = Color.WhiteSmoke;
+            InitializeControls();
+            UpdateReceiptPreview(); // Show an initial receipt
+        }
+
+        private void InitializeControls()
+        {
+            // --- Data for our parts ---
+            partsData = new Dictionary<string, int>
+            {
+                { "Motherboard", 5000 },
+                { "Mouse", 1500 },
+                { "Keyboard", 3000 },
+                { "Display", 14000 },
+                { "Processor", 25000 }
+            };
+
+            // --- Label for the ComboBox ---
+            Label partsLabel = new Label();
+            partsLabel.Text = "Parts";
+            partsLabel.Location = new Point(20, 20);
+            partsLabel.AutoSize = true;
+            this.Controls.Add(partsLabel);
+
+            // --- ComboBox for Parts Selection ---
+            partsComboBox = new ComboBox();
+            partsComboBox.Location = new Point(70, 18);
+            partsComboBox.Size = new Size(200, 25);
+            partsComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+            // Populate the ComboBox
+            foreach (var part in partsData)
+            {
+                partsComboBox.Items.Add($"{part.Key} ({part.Value})");
+            }
+            partsComboBox.SelectedIndex = 0; // Select the first item by default
+            partsComboBox.SelectedIndexChanged += (s, e) => UpdateReceiptPreview(); // Update receipt on change
+            this.Controls.Add(partsComboBox);
+
+            // --- Print Button ---
+            Button printBtn = new Button();
+            printBtn.Text = "Print";
+            printBtn.Location = new Point(140, 50);
+            printBtn.Size = new Size(100, 40);
+            printBtn.Click += PrintBtn_Click;
+            this.Controls.Add(printBtn);
+
+            // --- RichTextBox for Receipt Preview ---
+            receiptBox = new RichTextBox();
+            receiptBox.Location = new Point(20, 100);
+            receiptBox.Size = new Size(360, 450);
+            receiptBox.ReadOnly = true;
+            receiptBox.Font = new Font("Courier New", 10);
+            receiptBox.BorderStyle = BorderStyle.FixedSingle;
+            this.Controls.Add(receiptBox);
+        }
+
+        private string BuildReceipt(string partName, int partPrice)
+        {
+            int cashPaid = partPrice + 1000;
+            int change = cashPaid - partPrice;
+
+            string receipt = "";
+            receipt += CenterText("SHOP NAME") + "\n";
+            receipt += CenterText("Address: Lorem Ipsum, 23-10") + "\n";
+            receipt += CenterText("Telp. 11223344") + "\n";
+            receipt += CenterText("*************************") + "\n";
+            receipt += CenterText("CASH RECEIPT") + "\n";
+            receipt += CenterText("*************************") + "\n";
+            receipt += "\n";
+            receipt += "Description" + PadRight("Price", 38) + "\n";
+            receipt += partName + PadRight(partPrice.ToString(), 49 - partName.Length) + "\n";
+            receipt += "\n";
+            receipt += CenterText("*************************") + "\n";
+            receipt += "Total" + PadRight(partPrice.ToString(), 44) + "\n";
+            receipt += "Cash" + PadRight(cashPaid.ToString(), 45) + "\n";
+            receipt += "Change" + PadRight(change.ToString(), 42) + "\n";
+            receipt += CenterText("*************************") + "\n";
+            receipt += CenterText("THANK YOU!") + "\n";
+            receipt += CenterText("*************************") + "\n";
+            return receipt;
+        }
+
+        private void UpdateReceiptPreview()
+        {
+            if (partsComboBox.SelectedItem == null) return;
+
+            string selectedKey = partsComboBox.SelectedItem.ToString().Split(' ')[0];
+            int price = partsData[selectedKey];
+
+            receiptBox.Text = BuildReceipt(selectedKey, price);
+        }
+
+        private string CenterText(string text)
+        {
+            int totalWidth = 50;
+            int spaces = (totalWidth - text.Length) / 2;
+            return new string(' ', Math.Max(0, spaces)) + text;
+        }
+
+        private string PadRight(string text, int totalWidth)
+        {
+            return new string(' ', Math.Max(0, totalWidth - text.Length)) + text;
+        }
+
+        // --- THIS IS THE MODIFIED METHOD ---
+        // In your Print.cs file, replace the PrintBtn_Click method with this one.
+
+        private void PrintBtn_Click(object sender, EventArgs e)
+        {
+            // 1. Get all the necessary info
+            string currentReceipt = receiptBox.Text;
+            string selectedKey = partsComboBox.SelectedItem.ToString().Split(' ')[0];
+            int price = partsData[selectedKey];
+
+            // 2. Hide the current (Print) form
+            this.Hide();
+
+            // 3. Create and show the AccDeny form, now passing product info
+            AccDeny accDenyForm = new AccDeny(currentReceipt, selectedKey, price);
+            DialogResult result = accDenyForm.ShowDialog();
+
+            // 4. After AccDeny closes, check its result
+            if (result == DialogResult.Retry)
+            {
+                Login loginForm = new Login();
+                loginForm.Show();
+                this.Close();
+            }
+            else
+            {
+                this.Close();
+            }
+        }
+
+        // This method is no longer used by the Print button but can be kept for other purposes
+        private void PrintDoc_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            Font font = new Font("Courier New", 10);
+            e.Graphics.DrawString(receiptBox.Text, font, Brushes.Black, new PointF(20, 20));
+        }
+    }
+}
---
+++ technician.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace message
+{
+    partial class technician
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.btnChat = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnPrint = new System.Windows.Forms.Button();
+            //this.comboPrint = new System.Windows.Forms.ComboBox();
+            //this.Parts = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnSettings);
+            this.panel1.Controls.Add(this.btnHome);
+            this.panel1.Controls.Add(this.btnChat);
+            this.panel1.Location = new System.Drawing.Point(2, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(169, 642);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.Location = new System.Drawing.Point(19, 151);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(134, 38);
+            this.btnSettings.TabIndex = 2;
+            this.btnSettings.Text = "Settings";
+            this.btnSettings.UseVisualStyleBackColor = true;
+            // 
+            // btnHome
+            // 
+            this.btnHome.Location = new System.Drawing.Point(19, 101);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(134, 34);
+            this.btnHome.TabIndex = 1;
+            this.btnHome.Text = "Home";
+            this.btnHome.UseVisualStyleBackColor = true;
+            // 
+            // btnChat
+            // 
+            this.btnChat.Location = new System.Drawing.Point(19, 46);
+            this.btnChat.Name = "btnChat";
+            this.btnChat.Size = new System.Drawing.Size(134, 39);
+            this.btnChat.TabIndex = 0;
+            this.btnChat.Text = "Chat";
+            this.btnChat.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnPrint);
+            //this.panel2.Controls.Add(this.comboPrint);
+            this.panel2.Controls.Add(this.Parts);
+            this.panel2.Location = new System.Drawing.Point(177, -1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(478, 642);
+            this.panel2.TabIndex = 1;
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnPrint.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrint.Location = new System.Drawing.Point(184, 166);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(109, 44);
+            this.btnPrint.TabIndex = 8;
+            this.btnPrint.Text = "Print";
+            this.btnPrint.UseVisualStyleBackColor = false;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // comboPrint
+            // 
+            //this.comboPrint.FormattingEnabled = true;
+            //this.comboPrint.Items.AddRange(new object[] {
+            //"Matherboard (5000)",
+            //"Mouse(1500)",
+            //"Keyboard(3000)",
+            //"Display(14000)",
+            //"Processor(25000)"});
+            //this.comboPrint.Location = new System.Drawing.Point(124, 120);
+            //this.comboPrint.Name = "comboPrint";
+            //this.comboPrint.Size = new System.Drawing.Size(222, 24);
+            //this.comboPrint.TabIndex = 7;
+            // 
+            // Parts
+            // 
+            //this.Parts.AutoSize = true;
+            //this.Parts.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            //this.Parts.Location = new System.Drawing.Point(75, 123);
+            //this.Parts.Name = "Parts";
+            //this.Parts.Size = new System.Drawing.Size(43, 16);
+            //this.Parts.TabIndex = 6;
+            //this.Parts.Text = "Parts";
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(661, -1);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(373, 642);
+            this.panel3.TabIndex = 2;
+            // 
+            // technician
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1036, 639);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "technician";
+            this.Text = "technician";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.Button btnChat;
+        private System.Windows.Forms.Button btnPrint;
+        //private System.Windows.Forms.ComboBox comboPrint;
+        private System.Windows.Forms.Label Parts;
+    }
+}
